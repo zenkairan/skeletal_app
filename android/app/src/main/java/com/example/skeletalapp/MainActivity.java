5110431c2a0fcbf54fae8ac507d96b08b938154a
Klaus <ksf.klausfreire@gmail.com>
2019-05-28 14:17:17 -0300

    feat: splash screen for android

--- android/app/src/main/java/com/example/skeletalapp/MainActivity.java
+++ android/app/src/main/java/com/example/skeletalapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.skeletalapp;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    //Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
   }
 }
